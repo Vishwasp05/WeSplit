6081c68958e8a43933a8285e39a0d0abb16d18d3
Vishwas Sharma <ch0nky@protonmail.com>
2024-02-07 17:34:59 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vishwas Sharma on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
